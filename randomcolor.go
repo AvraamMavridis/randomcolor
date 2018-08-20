7c89e3c0099aa39219c66cea27bd225e30b92d40
AvraamMavridis <avraammauridis@gmail.com>
2018-08-20 14:01:34 +0200

    Feat: Generate random colors in HSV format

--- randomcolor.go
+++ randomcolor.go
@@ -16,18 +16,57 @@ type RGBColor struct {
 
 // HSVColor HSV Color Type
 type HSVColor struct {
-	hue        int
-	saturation int
-	value      int
+	hue        float64
+	saturation float64
+	value      float64
 }
 
 // GetHex Converts a decimal number to hex representations
-func GetHex(num int) string {
+func getHex(num int) string {
 	hex := fmt.Sprintf("%x", num)
 	if len(hex) == 1 {
 		hex = "0" + hex
 	}
 	return hex
+}
+
+func rgpToHSV(color RGBColor) HSVColor {
+	max := math.Max(float64(color.red), float64(color.green))
+	max = math.Max(max, float64(color.blue))
+	min := math.Min(float64(color.red), float64(color.green))
+	min = math.Min(min, float64(color.blue))
+	delta := max - min
+
+	red := float64(color.red)
+	green := float64(color.green)
+	blue := float64(color.blue)
+
+	var hue, saturation, value float64
+
+	if delta == 0 {
+		hue = 0
+	} else if red == max {
+		hue = float64(int(((green - blue) / delta)) % 6)
+	} else if green == max {
+		hue = (blue-red)/delta + 2
+	} else if blue == max {
+		hue = (red-green)/delta + 4
+	}
+
+	hue = math.Round(hue * 60)
+	if hue < 0 {
+		hue += 360
+	}
+
+	if max == 0 {
+		saturation = 0
+	} else {
+		saturation = (delta / max) * 100
+	}
+
+	value = max / 255 * 100
+
+	return HSVColor{hue, saturation, value}
 }
 
 // GetRandomColorInRgb Returns a random RGBColor
@@ -43,14 +82,12 @@ func GetRandomColorInRgb() RGBColor {
 // GetRandomColorInHex returns a random color in HEX format
 func GetRandomColorInHex() string {
 	color := GetRandomColorInRgb()
-	hex := "#" + GetHex(color.red) + GetHex(color.green) + GetHex(color.blue)
+	hex := "#" + getHex(color.red) + getHex(color.green) + getHex(color.blue)
 	return hex
 }
 
 // GetRandomColorInHSV returns a random color in HSV format
 func GetRandomColorInHSV() HSVColor {
 	color := GetRandomColorInRgb()
-	math.Max(float64(color.green), float64(color.blue))
-
-	return HSVColor{0, 0, 0}
+	return rgpToHSV(color)
 }
--- randomcolor_test.go
+++ randomcolor_test.go
@@ -1,17 +1,39 @@
 package randomcolor
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestHex(t *testing.T) {
-	hex := GetHex(10)
+	hex := getHex(10)
 	expected := "0a"
 	if hex != expected {
 		t.Errorf("Hex was convertion was incorrect, got: %s, want: %s.", hex, expected)
 	}
 
-	hex2 := GetHex(255)
+	hex2 := getHex(255)
 	expected2 := "ff"
 	if hex2 != expected2 {
 		t.Errorf("Hex convertion was incorrect, got: %s, want: %s.", hex2, expected2)
+	}
+}
+
+func TestRGBtoHex(t *testing.T) {
+	c := RGBColor{100, 130, 100}
+	hsv := rgpToHSV(c)
+
+	expected := 120.0
+	if hsv.hue != expected {
+		t.Errorf("Hue was not calculated correctly, got: %f, expected: %f.", hsv.hue, expected)
+	}
+
+	expected2 := 23.076923076923077
+	if hsv.saturation != expected2 {
+		t.Errorf("Saturation was not calculated correctly, got: %f, expected: %f.", hsv.saturation, expected2)
+	}
+
+	expected3 := 50.98039215686274
+	if hsv.value != expected3 {
+		t.Errorf("Saturation was not calculated correctly, got: %f, expected: %f.", hsv.value, expected3)
 	}
 }
